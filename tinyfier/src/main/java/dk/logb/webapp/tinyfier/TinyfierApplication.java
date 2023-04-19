2d2bdd9655e0828e57f4a9b9e46da469125d4f26
Jakob Bendsen <jbendsen@MBPtilhndeJakob.localdomain>
2023-04-19 16:49:14 +0200

    initial

---
+++ tinyfier/src/main/java/dk/logb/webapp/tinyfier/TinyfierApplication.java
@@ -0,0 +1,13 @@
+package dk.logb.webapp.tinyfier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyfierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyfierApplication.class, args);
+	}
+
+}
---
+++ tinyfier/src/test/java/dk/logb/webapp/tinyfier/TinyfierApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.logb.webapp.tinyfier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyfierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
